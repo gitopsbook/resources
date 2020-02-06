7a1873ac391b8005a783dddfb6701e4fd909c433
Todd Ekenstam <Todd_Ekenstam@intuit.com>
2020-02-05 21:53:48 -0800

    Update gitops-ci.sh script to reference sample-app

--- chapter-2/gitops-ci.sh
+++ chapter-2/gitops-ci.sh
@@ -1,27 +1,27 @@
-export VERSION=$(git rev-parse HEAD | cut -c1-7)                #A
-make build                                                      #B
+export VERSION=$(git rev-parse HEAD | cut -c1-7)
+make build
 make test
 
-export NEW_IMAGE="acme.com/guestbook:${VERSION}"                #C
+export NEW_IMAGE="gitopsbook/sample-app:${VERSION}"
 docker build -t ${NEW_IMAGE} .
 docker push ${NEW_IMAGE}
 
-git clone http://github.com/acme.com/guestbook.git              #D
-cd guestbook
+git clone http://github.com/gitopsbook/sample-app-deployment.git
+cd sample-app-deployment
 
-kubectl patch \                                                 #E
+kubectl patch \
   --local \
   -o yaml \
-  -f guestbook.yaml \
+  -f deployment.yaml \
   -p "spec:
         template:
           spec:
             containers:
-            - name: guestbook
+            - name: sample-app
               image: ${NEW_IMAGE}" \
-  > /tmp/newguestbook.yaml
-mv /tmp/newguestbook.yaml guestbook.yaml
+  > /tmp/newdeployment.yaml
+mv /tmp/newdeployment.yaml deployment.yaml
 
-git add . -m "Update guestbook to ${NEW_IMAGE}"                 #F
+git commit deployment.yaml -m "Update sample-app image to ${NEW_IMAGE}"
 git push
 
